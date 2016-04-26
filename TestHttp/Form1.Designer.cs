3689891c051861dc0e03bc90aad8be4f35b55126
FimJNE <jneypper@fim-medical.com>
2016-04-26 17:03:01 +0200

    First commit

---
+++ TestHttp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TestHttp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.listBoxEvts = new System.Windows.Forms.ListBox();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(403, 315);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.TabIndex = 0;
+            this.buttonQuit.Text = "Quitter";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // listBoxEvts
+            // 
+            this.listBoxEvts.FormattingEnabled = true;
+            this.listBoxEvts.Location = new System.Drawing.Point(12, 35);
+            this.listBoxEvts.Name = "listBoxEvts";
+            this.listBoxEvts.Size = new System.Drawing.Size(385, 303);
+            this.listBoxEvts.TabIndex = 1;
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(403, 286);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(75, 23);
+            this.buttonTest.TabIndex = 2;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 348);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.listBoxEvts);
+            this.Controls.Add(this.buttonQuit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.ListBox listBoxEvts;
+        private System.Windows.Forms.Button buttonTest;
+    }
+}
+
---
+++ TestHttp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+// http://stackoverflow.com/questions/4015324/http-request-with-post
+// Task + continue with
+
+namespace TestHttp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void SendPost()
+        {
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                   { "thing1", "hello" },
+                   { "thing2", "world" }
+                };
+
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private void SendGet()
+        {
+            using (var client = new HttpClient())
+            {
+                var responseString = client.GetStringAsync("http://www.example.com/recepticle.aspx");
+            }
+        }
+
+    }
+}
